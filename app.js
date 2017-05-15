539d6c5e0989048aafe984ee146a693aec3f7b23
Prasanna S <shenbagp@amazon.com>
2017-05-16 00:51:28 +0530

    Initial Commit - Adding v1.0

---
+++ app.js
@@ -0,0 +1,5 @@
+(function(){
+	var node = document.getElementById("pagelet_sidebar");
+	node.parentNode.removeChild(node);
+	document.getElementById("globalContainer").style.paddingRight = "0px"
+})();
